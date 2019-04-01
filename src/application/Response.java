c746feaa4b4517a9e9e4711cd9d29c5b9963efb3
Ruben, amir1337 <amir.parmback@gmail.com>
2019-04-01 17:50:42 +0200

    Response klassen!

--- src/application/Response.java
+++ src/application/Response.java
@@ -1,17 +1,33 @@
 package application;
-
+/**
+ * 
+ * @author Ruben, Amir
+ *
+ */
 public class Response {
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	private String type;
+	private Object responseObject;
+	/**
+	 * 
+	 * @param type
+	 * @param responseObject
+	 */
+	public Response(String type, Object responseObject) {
+		this.type = type;
+		this.responseObject = responseObject;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public Object getResponse() {
+		return responseObject;
+	}
 }
