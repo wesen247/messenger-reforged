eb336e8a322c6dbf13b92dc6d75fa8f33a95a556
Ruben <Ruben@DESKTOP-78AB8C6>
2019-04-02 17:17:18 +0200

    Lagt till skalen av Metoderna

--- src/application/ClientController.java
+++ src/application/ClientController.java
@@ -1,14 +1,81 @@
 package application;
 
 public class ClientController {
+	private LoginUI loginUI;
+	private	GroupChatUI groupChatUI;
+	private MainUI mainUI;
+	private CreateGroupUI createGroupUI;
+	private CreateNewUserUI createNewUserUI;
+	private PrivateChatUI privateChatUI;
+	private User user;
+	private Data data;
 
-	public static void main(String[] args) {
+	public ClientController(Data data) {
+		this.data = data;
+	}
+
+	public void start() {
 
 	}
-	private class ServerListener extends Thread {
+
+	public void initializeMainUI() {
+
+	}
+
+	public void initializeLoginUI() {
+
+	}
+
+	public void initializePrivateChatUI() {
+
+	}
+	
+	public void initializeCreateNewUserUI() {
+
+	}
+
+	public void initializeCreateGroupUI() {
+
+	}
+
+	public void initializeGroupChatUI() {
+
+	}
+	
+	public void createNewGroup(String groupName, String user) {
 		
+	}
+	
+	public void createNewUser(String name, String password) {
+		
+	}
+	
+	public void createGroupMessage(String message, String group) {
+		
+	}
+	
+	public void createPrivateMessage(String message, String reciver) {
+		
+	}
+	
+	public void logIn(String userName, String password) {
+		
+	}
+	
+	public void addGroupMember(String groupName, String user) {
+		
+	}
+
+	private class ServerListener extends Thread {
+
 		public void run() {
-			
+
 		}
+	}
+	
+	public static void main(String[] args) {
+		Data data = new Data();
+		ClientController controller = new ClientController(data);
+		controller.start();
 	}
 }
