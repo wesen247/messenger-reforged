eed30ef61773159d7d7a82c33a7759438dcb07e8
a <a@192.168.10.210>
2019-04-02 18:34:38 +0200

    Lagt till och ändrat klass.

--- src/application/MainUI.java
+++ src/application/MainUI.java
@@ -1,18 +1,23 @@
 package application;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-public class MainUI extends Application {
+public class MainUI<T> extends Application {
 	
 	private Parent content;
 	private FXMLLoader loader = new FXMLLoader();
 	private Parent root;
 	private Scene scene;
-
+	private ListView listViewGroups;
+	
+	
+	
 	public MainUI() {
 
 	}
@@ -25,7 +30,11 @@ public class MainUI extends Application {
 		stage.setTitle("Login");
 		stage.setScene(scene);
 		stage.show();
-
+		
+	
+		
+		
+		
 	}
 
 	public static void main(String[] args) {
