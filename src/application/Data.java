0a4bc2a7e887a2e43d03dfe0da5d1e36a36142f6
Amir <a@192.168.10.210>
2019-04-03 11:35:54 +0200

    Lagt till Data.java

--- src/application/Data.java
+++ src/application/Data.java
@@ -1,5 +1,57 @@
 package application;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Data {
+
+	private ObjectInputStream ois;
+	private Socket socket;
+
+	public Data() {
+
+		try {
+			socket = new Socket(InetAddress.getLocalHost(), 1234);
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	private class ServerListener extends Thread {
+
+		public void run() {
+			
+			try {
+			
+				ois = new ObjectInputStream(socket.getInputStream());
+			
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			while (true) {
+
+				try {
+				
+					Object object = ois.readObject();
+				
+				} catch (ClassNotFoundException | IOException e) {
+					e.printStackTrace();
+				}
+
+				
+				
+				
+			}
+
+		}
+
+	}
 
 }
